c9d94d2c7e5523b236182209a76340084f0d1a81
Adrian <adrianginergimenez@gmail.com>
2024-11-26 13:15:28 +0100

    feat: endpoints for messages

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const clientsRoutes = require('./clientsRoutes.js');
 const portfolioRoutes = require('./portfolioRoutes.js');
 const stateCardsRoutes = require('./stateCardRoutes.js');
 const openWorkRoutes = require('./openWorkRoutes.js');
+const messageRoutes = require('./messageRoutes.js');
 
 router.use('/api/artists', artistsRoutes);
 router.use('/api/clients', clientsRoutes);
 router.use('/api/portfolio', portfolioRoutes);
 router.use('/api/stateCards', stateCardsRoutes);
 router.use('/api/openWorks', openWorkRoutes);
+router.use('/api/messages', messageRoutes);
 
 module.exports = router
